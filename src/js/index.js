eee537c29d2096b399697ff5d0bc64c7a2dd957a
Elbert Corniell <elberthcorniell@gmail.com>
2020-12-24 10:56:46 -0400

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,52 +1,52 @@
 function Book(title, author, gender, read = false) {
-  this.title = title
-  this.author = author
-  this.gender = gender
-  this.read = read
+  this.title = title;
+  this.author = author;
+  this.gender = gender;
+  this.read = read;
 }
 
 Book.prototype.addBookToLibrary = function () {
-  myLibrary.push(this)
-  setBooks()
+  myLibrary.push(this);
+  setBooks();
 }
 
 Book.prototype.toggleRead = function () {
-  this.read = !this.read
-  return this
+  this.read = !this.read;
+  return this;
 }
 
 function mapBooks(callback) {
   myLibrary.map((book, index) => {
-    callback && callback(book, index)
-  })
+    callback && callback(book, index);
+  });
 }
 
 function deleteWithIndex(index, callback) {
-  myLibrary.splice(index, 1)
-  callback && callback()
-  setBooks()
+  myLibrary.splice(index, 1);
+  callback && callback();
+  setBooks();
 }
 
 function toggleReadWithIndex(index, callback) {
-  myLibrary[index] = myLibrary[index].toggleRead()
-  callback && callback()
-  setBooks()
+  myLibrary[index] = myLibrary[index].toggleRead();
+  callback && callback();
+  setBooks();
 }
 
 function getBooks() {
-  data = localStorage.getItem('myLibrary')
-  data = JSON.parse(data)
-  result = []
+  data = localStorage.getItem('myLibrary');
+  data = JSON.parse(data);
+  result = [];
   data.map(book => {
-    let { title, author, gender, read } = book
-    result.push(new Book(title, author, gender, read))
-  })
-  return result
+    let { title, author, gender, read } = book;
+    result.push(new Book(title, author, gender, read));
+  });
+  return result;
 }
 
 function setBooks() {
-  data = JSON.stringify(myLibrary)
-  localStorage.setItem('myLibrary', data)
+  data = JSON.stringify(myLibrary);
+  localStorage.setItem('myLibrary', data);
 }
 
 let myLibrary = getBooks();
