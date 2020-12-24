3919c0c48b4d915fd5129a4c73556a773571dccc
Elbert Corniell <elberthcorniell@gmail.com>
2020-12-24 10:20:26 -0400

    Delete function added

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,25 @@
 let myLibrary = [];
 
-function Book(name, author, gender) {
-    this.name = name
+function Book(title, author, gender) {
+    this.title = title
     this.author = author
     this.gender = gender
 }
 
-Book.prototype.addBookToLibrary = function() {
+Book.prototype.addBookToLibrary = function () {
     myLibrary.push(this)
 }
 
 function mapBooks(callback) {
-    myLibrary.map(book => {
-        callback && callback(book)
+    myLibrary.map((book, index) => {
+        callback && callback(book, index)
     })
+}
+
+function deleteWithIndex(index, callback) {
+    console.log(index)
+    myLibrary.splice(index, 1)
+    callback && callback()
 }
 
 new Book('Magic Kingdom', 'idk', 'magic').addBookToLibrary()
